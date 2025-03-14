8d3f378cde8426f1522372092deee64c33f6c030
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2025-03-14 11:33:17 +0530

    feat: add QuestionCard component and implement answer selection logic in quiz

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,14 @@ import { useEffect, useState } from "react";
 import { GameState } from "@/app/interface/quiz";
 import StartScreen from "@/app/components/start-screen";
 import Timer from "@/app/components/timer";
+import QuestionCard from "@/app/components/question-card";
+import { QUESTIONS } from "@/app/data/questions";
 
 export default function Home() {
   const [gameState, setGameState] = useState<GameState>("start");
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
   const [timeLeft, setTimeLeft] = useState<number>(30);
 
   useEffect(() => {
@@ -21,9 +26,31 @@ export default function Home() {
     return () => clearInterval(timer);
   }, [timeLeft, gameState]);
 
+
   const handleStart = () => {
     setGameState("playing");
     setTimeLeft(30);
+    setScore(0);
+    setCurrentQuestion(0);
+    setSelectedAnswer(null);
+  };
+
+  const handleAnswer = (index: number): void => {
+    setSelectedAnswer(index);
+    const isCorrect = index === QUESTIONS[currentQuestion].correct;
+
+    if (isCorrect) {
+      setScore((prev) => prev + 1);
+    }
+
+    setTimeout(() => {
+      if (currentQuestion < QUESTIONS.length - 1) {
+        setCurrentQuestion((prev) => prev + 1);
+        setSelectedAnswer(null);
+      } else {
+        setGameState("end");
+      }
+    }, 1500);
   };
 
   return (
@@ -33,6 +60,13 @@ export default function Home() {
         {gameState === "playing" && (
           <div className="p-8">
             <Timer timeLeft={timeLeft} />
+            <QuestionCard
+              question={QUESTIONS[currentQuestion]}
+              onAnswerSelect={handleAnswer}
+              selectedAnswer={selectedAnswer}
+              totalQuestions={QUESTIONS.length}
+              currentQuestion={currentQuestion}
+            />
           </div>
         )}
       </div>
